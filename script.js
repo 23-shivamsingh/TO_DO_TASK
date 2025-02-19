c04a5f8252d44708462dd5c629d1e07a1c7c4a52
23-shivamsingh <shivams2307@gmail.com>
2025-02-19 22:14:15 +0530

    Added images

--- script.js
+++ script.js
@@ -12,10 +12,29 @@ const addTask = () => {
 };
 
 const updateTaskslist = () => {
-    const taskList = document.querySelector("task-list");
-}
+    const taskList = document.getElementById("task-list");
+    taskList.innerHTML = "";
+
+    tasks.forEach((task, index) =>{
+        const listItem = document.createElement("li");
+
+        listItem.innerHTML = `
+        <div class="taskItem">
+        <div class="task ${task.completed ? "completed" : ""}">
+        <input type="checkbox" class="checkbox" ${task.completed ? "checked" : ""} />
+        <p>${task.text}</p>
+        </div>
+        <div class="icons">
+        <img src="./img/edit.png" onClick="EditTask(${index})"/>
+        <img src="./img/Delete.png" onClick="DeleteTask(${index})"/>
+        </div>
+        `;
+        listItem.addEventListener("change", () => toggleTaskcomplete(index));
+        taskList.append(listItem);
+    });
+};
 
 document.getElementById("newTask").addEventListener("click",function(e){
     e.preventDefault();
     addTask();
-});
+}); 
